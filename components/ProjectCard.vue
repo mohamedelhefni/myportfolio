220a1447fd63aa2290b47ebade414ab4ab337365
Mohamed Elhefni <49025288+mohamedelhefni@users.noreply.github.com>
2021-07-16 20:38:49 +0200

    fetch single project data

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="project bg-white text-gray-600 shadow rounded">
-    <img :src="project.image"class="w-full rounded max-h-52 h-48 object-cover " alt="">
+  <div class="project hover:shadow-lg bgwhite text-gray-600 shadow rounded">
+    <img
+      :src="project.image"
+      class="w-full rounded max-h-52 h-48 object-cover"
+      alt=""
+    />
     <div class="content p-4">
       <h3 class="font-bold text-xl text-gray-700">
-          <NuxtLink :to="{name: 'projects-id', params: {id: project.id}}">
-            {{project.title}}
-          </NuxtLink>
+        <NuxtLink :to="{ name: 'works-id', params: { id: project.id } }">
+          {{ project.title }}
+        </NuxtLink>
       </h3>
-      <p class="text-sm text-gray-500 ">{{project.excerpt}}</p>
+      <p class="text-sm text-gray-500">{{ project.excerpt }}</p>
     </div>
     <div class="categories flex flex-wrap justify-start items-center py-2">
-      <div class="category shadow bg-gray-200 roundedl px-3 py-1 text-sm font-semibold text-gray-700 mx-1 my-1" v-for="category in project.categories" :key="category.id" v-bind="{class: category.name}">
-        #{{category.name}}
+      <div
+        class="category shadow bg-gray-200 roundedl px-3 py-1 text-sm font-semibold text-gray-700 mx-1 my-1"
+        v-for="category in project.categories"
+        :key="category.id"
+        v-bind="{ class: category.name }"
+      >
+        #{{ category.name }}
       </div>
     </div>
   </div>
@@ -19,27 +28,28 @@
 
 <script>
 export default {
-  props: ['project']
-}
+  props: ["project"],
+};
 </script>
 
 <style>
 .materializecss {
-background: #ee6e73;
-color: white;}
+  background: #ee6e73;
+  color: white;
+}
 .gulp {
   background: #d34446;
-  color: white
+  color: white;
 }
 .xd {
   background: #450135;
-  color:white
+  color: white;
 }
 .firebase {
-background: #ffca2b
+  background: #ffca2b;
 }
-.vuetify{
-  background: #aeddff
+.vuetify {
+  background: #aeddff;
 }
 .php {
   background: #6675e0;
@@ -48,7 +58,6 @@ background: #ffca2b
 .tailwindcss {
   background: #06b6d4;
   color: white;
-
 }
 .javascript {
   background: #f0db4f;
--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="project">
-    hello from project
+  <div class="container p-5 mx-auto lg:w-3/4">
+    <div class="head flex flex-wrap items-center justify-between">
+      <h2 class="text-5xl font-bold">
+        {{ project.title }}
+      </h2>
+      <NuxtLink
+        class="font-semibold my-3 text-white hover:bg-blue-700 py-2 px-4 rounded bg-blue-500 text-white"
+        to="/projects"
+      >
+        Back
+      </NuxtLink>
+    </div>
+    <div v-html="project.body" class="content my-3 p-4"></div>
+    <h3 class="text-3xl font-bold my-5">Related Projects</h3>
+    <div
+      class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-3 mx-auto"
+    >
+      <ProjectCard
+        v-for="project in project.related_posts"
+        :key="project.id"
+        :project="project"
+      />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      project: {},
+    };
+  },
+  async fetch() {
+    this.project = await fetch(
+      `https://mohamedelhefni.000webhostapp.com/wp-json/portfolio/works/${this.$route.params.id}`
+    ).then((res) => res.json());
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.content a {
+  color: #4cacf6 !important;
+}
+</style>

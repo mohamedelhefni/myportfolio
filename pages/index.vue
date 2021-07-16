b89e1c62fcdf255f1d1e9bc2eb8e1ac33b1a3b29
Mohamed Elhefni <49025288+mohamedelhefni@users.noreply.github.com>
2021-07-16 22:45:29 +0200

    edit description

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,25 @@
 <template>
-    <div class="container p-5 my-4 mx-auto lg:w-3/4 ">
-      <h1 class="text-6xl font-bold mt-5" >
-        Hi there 👋
-      </h1>
-      <p class="description text-2xl my-3 p-4 mt-5 text-gray-600">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum doloremque sequi, in quam vitae nemo tempore, consequatur nisi, reprehenderit assumenda quos atque? Quos beatae esse eius voluptate rem, sapiente eligendi.
-      </p>
-    </div>
+  <div class="container p-5 my-4 mx-auto lg:w-3/4">
+    <h1 class="text-6xl font-bold mt-5">Welcome 👋</h1>
+    <p class="description text-2xl my-3 p-4 mt-5 text-gray-600">
+      My name is Mohamed Elhefni
+      <small class="text-gray-500">(@hefni101)</small><br />
+      I am a software engineer 👩‍💻, code addicted person ❤️, and a problem solver
+      I love to build software that makes my life easier and automate the boring
+      stuff with programming. <br />
+      intersted in competitive programming, Back-End
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   head: {
     bodyAttrs: {
-      class: "bg-gray-100"
-    }
-  }
-}
+      class: "bg-gray-100",
+    },
+  },
+};
 </script>
 
 <style>

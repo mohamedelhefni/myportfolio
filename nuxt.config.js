7b1642d0b227391fc6968c654d6d454490b21e51
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2021-10-18 15:42:23 +0200

    fix routing issue with netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,9 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
+  generate: {
+    fallback: true
+  },
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss

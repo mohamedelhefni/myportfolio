76fe332e71cebf8352aae5cfd1508e51ecf65b4f
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2022-01-17 20:48:34 +0200

    remove style tag form _slug.vue file

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -14,7 +14,7 @@
      <div class="content dark:bg-gray-700  hover:shadow-lg bg-white text-gray-600 dark:text-gray-300 shadow rounded my-3 w-full p-5 mt-10">
       <nuxt-content class="prose-lg dark:prose-invert" :document="post" />
      </div>
-   </div> 
+   </div>
 </template>
 
 
@@ -47,45 +47,3 @@ export default {
 
 };
 </script>
-
-<style >
- .content h1 {
-    @apply font-bold text-3xl my-3 mb-4 lg:text-5xl
-  }
- .content h2 {
-    @apply font-bold text-2xl my-2 mb-3 lg:text-4xl
- sm:text-3xl }
- .content h3 {
-    @apply font-bold text-xl my-2
-  }
- .content h4 {
-    @apply text-2xl
-  }
- .content h5 {
-    @apply text-xl
-  }
-  .content ul {
-    @apply list-inside list-disc
-  }
-  .content p {
-   @apply p-2 text-base lg:text-lg
-  }
-  .content a {
-    @apply text-blue-500 font-semibold
-  }
-  .content hr {
-    @apply w-3/6 mx-auto my-5
-  }
-  .content pre {
-   @apply rounded;
-   direction: ltr !important;
-  }
-
-  .content img {
-    @apply mx-auto my-5 max-h-64 
-  }
-  .content ul ul {
-    @apply ml-14
-  }
-  
-</style>
